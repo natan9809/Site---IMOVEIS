7f81342ea8c6ae4b158ded92296f91c3c2df44ea
Natan9809 <natanael_moreira@outlook.com.br>
2026-01-27 20:53:12 -0300

    dados de corretores

--- corretores.js
+++ corretores.js
@@ -19,4 +19,51 @@ const CORRETORES = [
       {tipo: "site", valor: "https://gustavocorretoritauna.com/"}
     ]
   },
+  {
+    id: 3,
+    nome: "Bruna Carmo",
+    foto: "img/corretores/brunacarmo.png",
+    cidade: "Itaúna - MG",
+    contatos: [
+      { tipo: "whatsapp", valor: "(37)998195989" },
+      {tipo: "site", valor: "https://www.brunacarmonegimobiliarios.com.br/"}
+    ]
+  },
+  {
+    id: 4,
+    nome: "Luiz Carvalho (GONZAGA)",
+    foto: "img/corretores/gonzaga.jpg",
+    cidade: "Itaúna - MG",
+    contatos: [
+      { tipo: "whatsapp", valor: "(37)999810092" }
+    ]
+  },
+  {
+    id: 5,
+    nome: "Jadiel Soares",
+    foto: "img/corretores/jadiel.png",
+    cidade: "Itaúna - MG",
+    contatos: [
+      { tipo: "whatsapp", valor: "(37)37998303181" },
+    ]
+  },
+  {
+    id: 5,
+    nome: "Eduardo El-Katib",
+    foto: "img/corretores/eduardo.png",
+    cidade: "Itaúna - MG",
+    contatos: [
+      { tipo: "whatsapp", valor: "(37)999210701" },
+    ]
+  },
+  {
+    id: 6,
+    nome: "Marina (SAMAR PRIME)",
+    foto: "img/corretores/samar.jpg",
+    cidade: "Itaúna - MG",
+    contatos: [
+      { tipo: "whatsapp", valor: "(37)998678378" },
+      {tipo: "site", valor: "https://www.samarprime.com.br/"}
+    ]
+  }
 ];
